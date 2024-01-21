c1b456eec0086e58a4273e4117110800279f1822
Amal Chulliyat Jose <amaljose1998@gmail.com>
2024-01-21 16:19:53 +0100

    rearranged the pettingzoo directory

--- Petting Zoo/Tutorial/env/api_test.py
+++ Petting Zoo/Tutorial/env/api_test.py
@@ -1,6 +1,7 @@
 from pettingzoo.test import parallel_api_test
 import custom_environment
 
+# test the environment to make sure it works as intended.
 if __name__ == "__main__":
     env = custom_environment.CustomEnvironment()
     parallel_api_test(env, num_cycles=1_000_000)
