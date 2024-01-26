84913a152d178a535efa2447274098237e7d5d3f
Amal Chulliyat Jose <amaljose1998@gmail.com>
2024-01-26 01:02:59 +0100

    folder names changed

--- Tutorials/Tutorial 2 simple adversarial transfer learning/dddqn.py
+++ Tutorials/Tutorial 2 simple adversarial transfer learning/dddqn.py
@@ -1,6 +1,6 @@
 import copy
 import numpy as np
-import torch
+import torch 
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
@@ -40,3 +40,32 @@ class dddQ_Net(nn.Module):
 		# Combining the value and advantage streams to get the Q-values
 		Q = V + (A - A.mean(dim=1, keepdim=True))
 		return Q
+	
+class ReplayBuffer():
+	def __init__(self, max_size=int(1e5)):
+		self.device = device
+		self.max_size = max_size
+		self.ptr = 0 #index of the last stored memmory
+		self.size = 0
+
+		self.state = torch.zeros((max_size, 4, 84, 84))
+		self.action = torch.zeros((max_size, 1), dtype=torch.int64)
+		self.reward = torch.zeros((max_size, 1))
+		self.next_state = torch.zeros((max_size, 4, 84, 84))
+		self.dw = torch.zeros((max_size, 1), dtype=torch.int8)
+
+	def add(self, state, action, reward, next_state, dw):
+		self.state[self.ptr] = state
+		self.action[self.ptr] = action
+		self.reward[self.ptr] = reward
+		self.next_state[self.ptr] = next_state
+		self.dw[self.ptr] = dw  # 0,0,0，...，1
+
+		self.ptr = (self.ptr + 1) % self.max_size
+		self.size = min(self.size + 1, self.max_size)
+
+	def sample(self, batch_size):
+		ind = np.random.choice((self.size-1), batch_size, replace=False)  # Time consuming, but no duplication
+		# ind = np.random.randint(0, (self.size-1), batch_size)  # Time effcient, might duplicates
+		return self.state[ind].to(self.device),self.action[ind].to(self.device),self.reward[ind].to(self.device),\
+			   self.next_state[ind].to(self.device),self.dw[ind].to(self.device)
